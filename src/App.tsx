000ef7ca491e5120478cc19a0750a9aa8ddf89ee
dancingKimDH <kimdonghyun0701@gmail.com>
2023-11-26 19:50:39 +0900

    firebase sdk setting

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
-
+import { useState } from 'react';
 import HomePage from 'pages/home';
 import Router from 'pages/components/Router';
 import { Layout } from 'pages/components/Layout';
-
+import { getAuth } from 'firebase/auth'
+import { app } from 'firebaseApp';
 
 
 function App() {
+
+  const auth = getAuth(app);
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(!!auth?.currentUser)
+  console.log(auth)
+
   return (
     <Layout>
-    <Router />
+      <Router />
     </Layout>
   );
 }
