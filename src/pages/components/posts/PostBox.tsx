c81d08f3962ee1b54039ef93f8e1d01c5452509d
dancingKimDH <kimdonghyun0701@gmail.com>
2023-12-05 21:02:59 +0900

    post delete

--- src/pages/components/posts/PostBox.tsx
+++ src/pages/components/posts/PostBox.tsx
@@ -1,9 +1,12 @@
 import { PostProps } from "pages/home";
 import { AiFillHeart } from "react-icons/ai"
 import { FaRegComment, FaUserCircle } from "react-icons/fa"
-import { Link } from "react-router-dom"
+import { Link, Navigate, useNavigate } from "react-router-dom"
 import { useContext } from "react";
 import AuthContext from "../context/AuthContext";
+import { deleteDoc, doc } from "firebase/firestore";
+import { db } from "firebaseApp";
+import { toast } from "react-toastify";
 
 interface PostBoxProps {
     post: PostProps;
@@ -11,8 +14,16 @@ interface PostBoxProps {
 
 export default function PostBox({ post }: PostBoxProps) {
 
+    const navigate = useNavigate();
     const { user } = useContext(AuthContext);
-    const handleDelete = () => { }
+    const handleDelete = async () => {
+        const confirm = window.confirm("해당 게시글을 삭제하시겠습니까?");
+        if(confirm){
+            await deleteDoc(doc(db, "posts", post.id));
+            toast.success("게시글을 삭제하였습니다");
+            navigate("/");
+        }
+     }
 
     return (
 
@@ -28,8 +39,8 @@ export default function PostBox({ post }: PostBoxProps) {
                     <div className='post__box-content'>{post?.content}</div>
                 </div>
             </Link>
+            
             <div className='post__box-footer'>
-
                 {user?.uid === post?.uid && (
                     <>
                         <button type='button' className='post__delete' onClick={handleDelete}>Delete</button>
@@ -40,8 +51,6 @@ export default function PostBox({ post }: PostBoxProps) {
                         <button type='button' className='post__comments'><FaRegComment /> {post?.comments?.length || 0} </button>
                     </>
                 )}
-
-
             </div>
         </div>
 
