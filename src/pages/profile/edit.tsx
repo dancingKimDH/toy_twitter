db14eb9d7cedec4c609a5cb227b91c8e3fb240b5
dancingKimDH <kimdonghyun0701@gmail.com>
2023-12-11 22:00:30 +0900

    profile edit

--- src/pages/profile/edit.tsx
+++ src/pages/profile/edit.tsx
@@ -1,3 +1,44 @@
+import PostHeader from "pages/components/posts/Header";
+import { useState } from "react";
+import { FiImage } from "react-icons/fi";
+
 export default function ProfileEdit() {
-    return <h1>Profile Edit</h1>
+
+    const onchange = () => { }
+
+    const handleFileUpload = () => { }
+
+    const handleDeleteImage = () => { }
+    
+    const [imageUrl, setImageUrl] = useState<string>("");
+
+    const [displayName, setDisplayName] = useState<string>("");
+
+    return <div className="post">
+        <PostHeader />
+        <form action="" className="post-form">
+            <div className="post-form__profile">
+                <input type="text" name="displayName" id="" className="post-form__input" placeholder="이름"
+                    onChange={onchange} value={displayName} />
+
+                    {imageUrl && (
+                        <div className="post-form__attachment">
+                            <img src={imageUrl} alt="attachment" width={100} height={100} />
+                            <button type="button" className="post-form__clear-btn" onClick={handleDeleteImage}>Delete</button>
+                        </div>
+                    )}
+
+                <div className="post-form__submit-area">
+                    <div className="post-form__image-area">
+                        <label htmlFor="file-input" className="post-form__file">
+                            <FiImage className="post-form__file-icon" />
+                        </label>
+                    </div>
+                    <input type="file" name="file-input" id="file-input" accept="image/*" onChange={handleFileUpload} className="hidden"/>
+                    <input type="submit" value="프로필 수정" className="post-form__submit-btn" />
+                </div>
+
+            </div>
+        </form>
+    </div>
 }
--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,3 +1,68 @@
+import { collection, onSnapshot, orderBy, query, where } from "firebase/firestore";
+import { db } from "firebaseApp";
+import AuthContext from "pages/components/context/AuthContext";
+import PostBox from "pages/components/posts/PostBox";
+import { PostProps } from "pages/home";
+import { useContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 export default function ProfilePage() {
-    return <h1>Profile</h1>
+
+    const [posts, setPosts] = useState<PostProps[]>([]);
+
+    const { user } = useContext(AuthContext);
+
+    const navigate = useNavigate();
+
+    const PROFILE_DEFAULT_URL = "/logo192.png";
+
+    useEffect(() => {
+        if (user) {
+            let postsRef = collection(db, "posts");
+            let postsQuery = query(postsRef, where("uid", "==", user.uid), orderBy("createdAt", "desc"));
+
+            onSnapshot(postsQuery, (snapshot) => {
+                let dataObj = snapshot.docs.map((doc) => ({
+                    ...doc.data(),
+                    id: doc?.id
+                }))
+                setPosts(dataObj as PostProps[]);
+            })
+        }
+    })
+
+    return (
+
+
+        <div className="home">
+            <div className="home__top">
+                <div className="home__title">Profile</div>
+                <div className="profile">
+                    <img src={user?.photoURL || PROFILE_DEFAULT_URL} alt="profile" className="profile__image" />
+                    <button type="button" className="profile__btn" onClick={() => navigate("/profile/edit")}>프로필 수정</button>
+                </div>
+
+                <div className="profile__text">
+                    <div className="profile__name">{user?.displayName || "사용자님"}</div>
+                    <div className="profile__name">{user?.email}</div>
+
+                </div>
+
+                <div className="home__tabs">
+                    <div className="home__tab home__tab--active">For You</div>
+                    <div className="home__tab home__tab--active">Likes</div>
+                </div>
+
+                <div className="post">
+                    {posts?.length > 0 ? (posts?.map((post) => <PostBox post={post} key={post.id} />)) :
+                        <div className='post__no-posts'>
+                            <div className='post__text'>
+                                게시글이 없습니다.
+                            </div>
+                        </div>}
+                </div>
+
+            </div>
+        </div>
+    )
 }
