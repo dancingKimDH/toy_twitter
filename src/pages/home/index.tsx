5099ffb5af0e69a436f1efe2475be784d642c600
dancingKimDH <kimdonghyun0701@gmail.com>
2024-01-01 13:18:08 +0900

    followed posts list

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,7 @@
 import { Link } from 'react-router-dom';
 import PostForm from 'pages/components/posts/PostForm';
 import PostBox from 'pages/components/posts/PostBox';
-import { useEffect, useState, useContext } from "react";
+import { useEffect, useState, useContext, useCallback } from "react";
 import AuthContext from 'pages/components/context/AuthContext';
 import { collection, query, where, onSnapshot, orderBy, doc } from 'firebase/firestore';
 import { db } from 'firebaseApp';
@@ -20,18 +20,43 @@ export interface PostProps {
     imageUrl?: string;
 }
 
+interface UserProps {
+    id: string;
+}
 
+type tabType = "all" | "following";
 
 export default function HomePage() {
 
     const [posts, setPosts] = useState<PostProps[]>([]);
 
-    const {user} = useContext(AuthContext);
+    const { user } = useContext(AuthContext);
 
-    useEffect(()=>{
-        if(user) {
+    const [activeTab, setActiveTab] = useState<tabType>("all");
+
+    const [followingPosts, setFollowingPosts] = useState<PostProps[]>([]);
+
+    const [followingIds, setFollowingIds] = useState<string[]>([""]);
+
+    const getFollowingIds = useCallback(async () => {
+        if (user?.uid) {
+            const ref = doc(db, "following", user?.uid);
+            onSnapshot(ref, (doc) => {
+                setFollowingIds([""]);
+                doc?.data()?.users.map((user: UserProps) => setFollowingIds((prev: string[]) => prev ? [...prev, user?.id] : []))
+            })
+        }
+    }, [user?.uid])
+
+    useEffect(() => {
+        if (user?.uid) getFollowingIds();
+    }, [getFollowingIds, user?.uid])
+
+    useEffect(() => {
+        if (user) {
             let postsRef = collection(db, "posts");
             let postsQuery = query(postsRef, orderBy("createdAt", "desc"));
+            let followingQuery = query(postsRef, where("uid", "in", followingIds), orderBy("createdAt", "desc"))
 
             onSnapshot(postsQuery, (snapshot) => {
                 let dataObj = snapshot.docs.map((doc) => ({
@@ -40,8 +65,16 @@ export default function HomePage() {
                 }))
                 setPosts(dataObj as PostProps[]);
             })
+
+            onSnapshot(followingQuery, (snapshot) => {
+                let dataObj = snapshot.docs.map((doc) => ({
+                    ...doc.data(),
+                    id: doc?.id
+                }))
+                setFollowingPosts(dataObj as PostProps[]);
+            })
         }
-    })
+    }, [followingIds, user])
 
     const handleDelete = () => { }
         ; return (
@@ -50,28 +83,46 @@ export default function HomePage() {
                     <div className="home__top">
                         <div className="home__title">Home</div>
                         <div className="home__tabs">
-                            <div className="home__tab home__tab--active">For You</div>
-                            <div className="home__tab home__tab--active">Following</div>
+                            <div className={`home__tab ${activeTab === "all" && "home__tab--active"}`} onClick={() => { setActiveTab("all") }}>For You</div>
+                            <div className={`home__tab ${activeTab === "following" && "home__tab--active"}`} onClick={() => { setActiveTab("following") }}>Following</div>
                         </div>
                     </div>
                 </div>
 
                 {/* post form */}
                 <PostForm />
-
-                {/* tweet posts */}
-                <div className='post'>
-                    {posts?.length > 0 ? posts?.map((post) => (
-                        <PostBox post={post} key={post?.id} />
-                    )) :
-                        <div className='post__no-posts'>
-                            <div className='post__text'>
-                                게시글이 없습니다.
+                {activeTab === "all" && (
+                    // tweet posts
+                    <div className='post'>
+                        {posts?.length > 0 ? posts?.map((post) => (
+                            <PostBox post={post} key={post?.id} />
+                        )) :
+                            <div className='post__no-posts'>
+                                <div className='post__text'>
+                                    게시글이 없습니다.
+                                </div>
                             </div>
-                        </div>
-                    }
+                        }
 
-                </div>
+                    </div>
+                )}
+
+                {activeTab === "following" && (
+                    // tweet posts
+                    <div className='post'>
+                        {followingPosts?.length > 0 ? followingPosts?.map((post) => (
+                            <PostBox post={post} key={post?.id} />
+                        )) :
+                            <div className='post__no-posts'>
+                                <div className='post__text'>
+                                    게시글이 없습니다.
+                                </div>
+                            </div>
+                        }
+
+                    </div>
+                )}
+
             </div>
         )
 }
