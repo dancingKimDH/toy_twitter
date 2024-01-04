d869c704ab291489d803cd587d28af4cf95cbd0b
dancingKimDH <kimdonghyun0701@gmail.com>
2024-01-04 19:44:48 +0900

    menu bar language

--- src/pages/components/menu.tsx
+++ src/pages/components/menu.tsx
@@ -8,27 +8,28 @@ import { getAuth, signOut } from "firebase/auth";
 import { app } from "firebaseApp";
 import { toast } from "react-toastify";
 import { AiOutlineSearch } from "react-icons/ai";
+import useTranslation from "hooks/useTranslation";
 
 export default function MenuList() {
 
     const navigate = useNavigate();
     const { user } = useContext(AuthContext);
-
+    const t = useTranslation();
 
     return <div className="footer">
         <div className="footer__grid">
-            <button type="button" onClick={() => navigate("/")}><FaHouse /> Home</button>
-            <button type="button" onClick={() => navigate("/profile")}><CgProfile /> Profile</button>
-            <button type="button" onClick={() => navigate("/search")}><AiOutlineSearch /> Search</button>
-            <button type="button" onClick={() => navigate("/notifications")}><IoMdNotifications/> Notification</button>
+            <button type="button" onClick={() => navigate("/")}><FaHouse /> {t("MENU_HOME")}</button>
+            <button type="button" onClick={() => navigate("/profile")}><CgProfile /> {t("MENU_PROFILE")}</button>
+            <button type="button" onClick={() => navigate("/search")}><AiOutlineSearch /> {t("MENU_SEARCH")} </button>
+            <button type="button" onClick={() => navigate("/notifications")}><IoMdNotifications/> {t("MENU_NOTI")} </button>
             {user === null ?
-                <button type="button" onClick={() => navigate("/users/login")}><IoMdLogIn /> LogIn</button>
+                <button type="button" onClick={() => navigate("/users/login")}><IoMdLogIn /> {t("MENU_LOGIN")} </button>
                 :
                 <button type="button" onClick={async () => {
                     const auth = getAuth(app);
                     await signOut(auth);
                     toast.success("로그아웃되었습니다.")
-                }}><IoIosLogOut /> LogOut</button>
+                }}><IoIosLogOut /> {t("MENU_LOGOUT")} </button>
             }
         </div>
     </div>
