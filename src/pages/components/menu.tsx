2a7e747473138cb332f412282294b50524c4071a
dancingKimDH <kimdonghyun0701@gmail.com>
2024-01-05 13:12:54 +0900

    responsive screen

--- src/pages/components/menu.tsx
+++ src/pages/components/menu.tsx
@@ -18,18 +18,24 @@ export default function MenuList() {
 
     return <div className="footer">
         <div className="footer__grid">
-            <button type="button" onClick={() => navigate("/")}><FaHouse /> {t("MENU_HOME")}</button>
-            <button type="button" onClick={() => navigate("/profile")}><CgProfile /> {t("MENU_PROFILE")}</button>
-            <button type="button" onClick={() => navigate("/search")}><AiOutlineSearch /> {t("MENU_SEARCH")} </button>
-            <button type="button" onClick={() => navigate("/notifications")}><IoMdNotifications/> {t("MENU_NOTI")} </button>
+            <button type="button" onClick={() => navigate("/")}><FaHouse />
+                <span className="footer__grid-text">{t("MENU_HOME")}</span></button>
+            <button type="button" onClick={() => navigate("/profile")}><CgProfile />
+                <span className="footer__grid-text">{t("MENU_PROFILE")}</span></button>
+            <button type="button" onClick={() => navigate("/search")}><AiOutlineSearch />
+                <span className="footer__grid-text">{t("MENU_SEARCH")}</span> </button>
+            <button type="button" onClick={() => navigate("/notifications")}><IoMdNotifications />
+                <span className="footer__grid-text">{t("MENU_NOTI")}</span> </button>
             {user === null ?
-                <button type="button" onClick={() => navigate("/users/login")}><IoMdLogIn /> {t("MENU_LOGIN")} </button>
+                <button type="button" onClick={() => navigate("/users/login")}><IoMdLogIn />
+                    <span className="footer__grid-text">{t("MENU_LOGIN")}</span> </button>
                 :
                 <button type="button" onClick={async () => {
                     const auth = getAuth(app);
                     await signOut(auth);
                     toast.success("로그아웃되었습니다.")
-                }}><IoIosLogOut /> {t("MENU_LOGOUT")} </button>
+                }}><IoIosLogOut />
+                    <span className="footer__grid-text">{t("MENU_LOGOUT")}</span> </button>
             }
         </div>
     </div>
